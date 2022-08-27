43f24d580c5e2a1439460b7ba5887ffc1cde598f
YUTA-KON <dorata.1007@gmail.com>
2022-08-27 21:01:23 +0900

    add data directory

--- app/image_collection.py
+++ app/image_collection.py
@@ -8,26 +8,28 @@ import chromedriver_binary
 # File Name
 FILENAME = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/image/"+"1"+".png")
 
+#オプションの設定
+options = Options()
+options.binary_location = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+options.add_argument('--headless')
+
 # set driver and url
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 url = 'http://localhost:8080/map?x=36.151&y=36.151'
-#driver.get(url)
 
 # get width and height of the page
 #w = driver.execute_script("return document.body.scrollWidth;")
 #h = driver.execute_script("return document.body.scrollHeight;")
+#driver.set_window_size(w,h)
 
 # set window size
-#driver.set_window_size(w,h)
 driver.set_window_size(900,900)
 driver.get(url)
 time.sleep(1)
 
 # Get Screen Shot
-#driver.save_screenshot(FILENAME)
 png = driver.find_element_by_id('map').screenshot_as_png
 with open('data/image/1.png', 'wb') as f:
         f.write(png)
 
-# Close Web Browser
 driver.quit()
