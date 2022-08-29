f1fc7adda673a1da2341f7942c2b899e8cb9f1c5
YUTA-KON <dorata.1007@gmail.com>
2022-08-29 12:07:28 +0900

    Add Program For Preparing Datasets

---
+++ app/MakeDataset.java
@@ -0,0 +1,27 @@
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import kon.lib.coord.CoordDouble;
+import kon.lib.col.Col;
+
+public class MakeDataset{
+    public static void main(String[] args){
+        //int[] col_nums = {0,1};
+        //CoordDouble jiko = new CoordDouble("../data/jinshin_zahyou_short.csv", col_nums);
+        //jiko.readData(",");
+        //jiko.show();
+        Col target = new Col("../data/line_short.txt",0);
+        target.readData(" ");
+        for(int i=0; i<target.coords.size();i++){
+            Path original_path= Paths.get("../data/image/"+(i+1)+".png");
+            Path dataset_path= Paths.get("../data/datasets/road_shape/"+target.coords.get(i)+"/"+(i+1)+".png");
+            try{
+                Files.copy(original_path, dataset_path);
+            }catch(IOException e){
+                System.out.println("データセットのコピーに失敗しました(´・ω・)");
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- initializer.sh
+++ initializer.sh
@@ -2,3 +2,34 @@
 
 #pip3 install selenium==3.141.0
 #pip3 install chromedriver-binary==104.0.5112.79.0
+mkdir data/image
+mkdir data/model
+
+##########################//
+#dirctories for datasets#//
+########################//
+mkdir data/datasets
+#1
+#road_shape
+mkdir data/datasets/road_shape
+mkdir data/datasets/road_shape/0
+mkdir data/datasets/road_shape/1
+mkdir data/datasets/road_shape/7
+mkdir data/datasets/road_shape/11
+mkdir data/datasets/road_shape/12
+mkdir data/datasets/road_shape/13
+mkdir data/datasets/road_shape/14
+mkdir data/datasets/road_shape/21
+#2
+#road_liner
+mkdir data/datasets/road_liner
+mkdir data/datasets/road_liner/0
+mkdir data/datasets/road_liner/1
+mkdir data/datasets/road_liner/2
+mkdir data/datasets/road_liner/3
+mkdir data/datasets/road_liner/0
+mkdir data/datasets/road_liner/0
+mkdir data/datasets/road_liner/6
+mkdir data/datasets/road_liner/7
+mkdir data/datasets/road_liner/8
+mkdir data/datasets/road_liner/9
